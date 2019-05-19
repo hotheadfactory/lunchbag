3307cabd631aca44c31eb301c5c6b0cab31bc06a
parkjuil <wndlf0895@naver.com>
2019-05-19 15:10:23 +0900

    pagemove

--- app/src/main/java/com/lunchbag/lunchbag/mainPageActivity.java
+++ app/src/main/java/com/lunchbag/lunchbag/mainPageActivity.java
@@ -5,7 +5,12 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.content.Intent;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class mainPageActivity extends AppCompatActivity {
     private TextView mTextMessage;
@@ -31,6 +36,14 @@ public class mainPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        Button button;
+        button =(Button)findViewById(R.id.spinButton);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),reviewActivity.class);
+                startActivity(intent);
+            }
+        });
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
---
+++ app/src/main/java/com/lunchbag/lunchbag/reviewActivity.java
@@ -0,0 +1,13 @@
+package com.lunchbag.lunchbag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class reviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+    }
+}
