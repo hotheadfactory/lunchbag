8d9400edf36e6543a16009e43f98d6bd591ae526
Andy H. Jung <commaniakr@gmail.com>
2019-06-18 10:53:52 +0900

    최종 완성

---
+++ app/src/main/java/com/lunchbag/lunchbag/java/vocMenuActivity.java
@@ -0,0 +1,61 @@
+package com.lunchbag.lunchbag.java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class vocMenuActivity extends AppCompatActivity {
+
+    Button emailSend;
+    EditText titleText;
+    EditText contentText;
+    Button restaurantButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voc_menu);
+
+        emailSend = (Button) findViewById(R.id.emailSend);
+        titleText = (EditText) findViewById(R.id.titleText);
+        contentText = (EditText) findViewById(R.id.contentText);
+        restaurantButton = (Button) findViewById(R.id.restaurantButton);
+
+        restaurantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), vocRestaurantActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        emailSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String title = titleText.getText().toString();
+                String content = contentText.getText().toString();
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                try{
+                    emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"wn950430@gmail.com"});
+                    emailIntent.putExtra(Intent.EXTRA_SUBJECT, title);
+                    emailIntent.putExtra(Intent.EXTRA_TEXT, content);
+                    emailIntent.setType("text/html");
+                    emailIntent.setPackage("com.google.android.gm");
+                    if(emailIntent.resolveActivity(getPackageManager())!=null)
+                        startActivity(emailIntent);
+                }catch (Exception e){
+                    e.printStackTrace();
+
+                    emailIntent.setType("text/html");
+                    emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"wn950430@gmail.com"});
+
+                    startActivity(Intent.createChooser(emailIntent, "Send Email"));
+                }
+            }
+        });
+    }
+}
